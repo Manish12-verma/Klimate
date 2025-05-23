b34c8f6b5ec694649ddcb77ebef02e35b2f630f5
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-05-24 01:38:45 +0530

    project setup completed

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh ">
+      
+    </div>
+  )
+}
+
+export default App
