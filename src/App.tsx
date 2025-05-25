3cf294a1a98170fc0c146613bde5f05f0e44ec3b
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-05-25 19:52:39 +0530

    created queries for weather API

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,16 @@ import { Layout } from "./components/layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
  
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 10, // 10 minutes
+      retry:false,
+      refetchOnWindowFocus: false, // Disable refetching on window focus
+    }
+  }
+});
  
 function App() {
   return (
--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,8 +1,105 @@
+import LoadingSkeleton from "@/components/LoadingSkeleton";
+import { Button } from "@/components/ui/button"
+import { useGeoLocation } from "@/hooks/UseGeoLocation";
+import { MapPin, RefreshCw } from "lucide-react"
+import { AlertTriangle } from "lucide-react"
+ 
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/UseWeather";
+
 
 const WeatherDashboard = () => {
+
+const {coordinates,error:locationError,getLocation,isLoading:locationLoading} = useGeoLocation();
+ 
+
+const locationQuery = useReverseGeocodeQuery(coordinates);
+const forecastQuery = useForecastQuery(coordinates);
+const weatherQuery = useWeatherQuery(coordinates);
+
+
+  const handleRefresh = () => {
+    getLocation();
+    if(coordinates) {
+     weatherQuery.refetch();
+      forecastQuery.refetch();
+      locationQuery.refetch();  
+      
+    }
+  };
+
+  if(locationLoading) {
+    return <LoadingSkeleton/>
+  }
+
+  if(locationError) {
+   return (
+   <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle>Location Error</AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+       <p>{locationError}</p>
+       <Button onClick={getLocation} variant={'outline'} className="w-fit">
+        <MapPin className="h-4 w-4 mr-2" />
+         Enable Location
+       </Button>
+      </AlertDescription>
+    </Alert>
+   );
+  }
+
+  
+  if(!coordinates) {
+   return (<Alert variant="destructive">
+      <AlertTitle>Location Required</AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+       <p>Please enable location access to see your local weather.</p>
+       <Button onClick={getLocation} variant={'outline'} className="w-fit">
+        <MapPin className="h-4 w-4 mr-2" />
+         Enable Location
+       </Button>
+      </AlertDescription>
+    </Alert>
+   );
+  }
+  
+  const locationName = locationQuery.data?.[0];
+  
+  if(weatherQuery.error || forecastQuery.error){
+    return (
+        <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle> Error</AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+       <p>Failed to fetch weather data. Please try again.</p>
+       <Button onClick={getLocation} variant={'outline'} className="w-fit">
+        <RefreshCw className="h-4 w-4 mr-2" />
+         retry
+       </Button>
+      </AlertDescription>
+    </Alert>
+    )
+  }
+
+  if(!weatherQuery.data || !forecastQuery.data ) {
+    return <LoadingSkeleton/>
+  } 
+
   return (
-    <div>
-       Weather dashboard
+    <div className="space-y-4 ">
+      <div className="flex items-center justify-between ">
+        <h1 className="text-xl font-bold tracking-tight">My location</h1>
+        <Button variant={"outline"} size={'icon'} onClick={handleRefresh}
+        disabled={weatherQuery.isFetching || forecastQuery.isFetching}
+        >
+          <RefreshCw className={`h-4 w-4 ${weatherQuery.isFetching ? "animate-spin":""}`}/>
+        </Button>
+      </div>
+           {/* current and hourly weather data will be displayed here */}
     </div>
   )
 }
