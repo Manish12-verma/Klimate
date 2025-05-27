78017349e330411e89311cb34609efdb9a2ed9ca
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-05-27 12:56:00 +0530

    created favorite hook

---
+++ src/hooks/useFavorite.ts
@@ -0,0 +1,80 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {useLocalStorage} from './UseLocalStorage';
+
+interface FavoriteCity {
+    id: string;
+    lat: number;
+    lon: number;
+    name:string;
+    country: string;
+    state?: string;
+    addedAt: number;
+}
+
+export function useFavorite() {
+    const [favorites,setFavorites] =   useLocalStorage<FavoriteCity[]>(
+        'favorites',
+        []
+    );
+  
+    const queryClient =useQueryClient();
+
+   const favoritesQuery =  useQuery({
+        queryKey: ['favorites'],
+        queryFn: () => favorites,
+        initialData: favorites,
+        staleTime: Infinity, 
+    });
+
+    const addFavorite = useMutation({
+          mutationFn:async (
+            city : Omit<FavoriteCity,"id" | "addedAt">
+          )=>{
+           const newFavorite:FavoriteCity={
+            ...city, 
+            id: `${city.lat}-${city.lon}-${Date.now()}`,
+            addedAt: Date.now(),
+           };
+
+            const exists = favorites.some((fav)=>fav.id === newFavorite.id);
+            if(exists) return favorites;
+
+
+            const newFavorites = [ ...favorites,newFavorite].slice(0, 10);
+            setFavorites(favorites);
+
+            return newFavorites;
+          },
+          onSuccess:()=>{
+            queryClient.invalidateQueries({
+                queryKey: ["favorites"],
+                
+            })
+          }
+    });
+
+    const removeFavorite = useMutation({
+        mutationFn: async (cityId:string) => {
+            const updatedFavorites = favorites.filter(
+                (city) => city.id !== cityId
+            );
+            setFavorites(updatedFavorites);
+            return updatedFavorites;
+        },
+        onSuccess:()=>{
+            queryClient.invalidateQueries({
+                queryKey: ["favorites"],
+                
+            })
+          }
+    });
+
+    return {
+       favorites: favoritesQuery.data,
+       addFavorite,
+       removeFavorite,
+       isFavorite:(lat:number,lon:number)=>
+        favorites.some((city) => city.lat === lat && city.lon === lon),
+
+    }
+}
