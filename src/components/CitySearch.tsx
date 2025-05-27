c56e726546e991409186ea4c56d000ccb39d2330
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-05-27 12:04:24 +0530

    created search history feature

--- src/components/CitySearch.tsx
+++ src/components/CitySearch.tsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
 import { Button } from "./ui/button"
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from "./ui/command"
-import { Loader2, Search } from "lucide-react";
+import { Clock, Loader2, Search, XCircle } from "lucide-react";
 import { useLocationSearchQuery } from "@/hooks/UseWeather";
 import { useNavigate } from "react-router-dom";
+import { useSearchHistory } from "@/hooks/useSearchHistory";
+import { format } from "date-fns";
 
 
 const CitySearch = () => {
     const [open, setOpen] = useState(false);
     const [query, setQuery] = useState("");
     const{data:locations,isLoading} = useLocationSearchQuery(query)
+    const {history,clearHistory,addToHistory} = useSearchHistory();
+
     const navigate = useNavigate();
 
     const handleSelect = (cityData:string)=>{
           const [lat, lon, name, country] = cityData.split("|");
-        
+          
+          addToHistory.mutate({
+             query,
+             name,
+             lat: parseFloat(lat),
+             lon: parseFloat(lon),
+             country
+          })
           //add to search history
           setOpen(false);
           navigate(`/city/${name}??lat=${lat}&lon=${lon}` )
@@ -22,7 +33,7 @@ const CitySearch = () => {
 
   return ( 
     <>
-      <Button onClick={()=>{setOpen(true) }} variant={"outline"} className="relative w-full justify-start text-sm text-muted-foreground sm:pr-12  md:w-40 lg:w-64">
+      <Button onClick={()=>{setOpen(true) }} variant={"outline"} className=" cursor-pointer relative w-full justify-start text-sm text-muted-foreground sm:pr-12  md:w-40 lg:w-64">
         <Search className="h-4 w-4 mr-2" />
         Search cities....
       </Button>
@@ -30,17 +41,52 @@ const CitySearch = () => {
       <CommandInput placeholder="Search cities... " value={query} onValueChange={setQuery} />
       <CommandList>
         {query.length>2 && ! isLoading && ( <CommandEmpty >No Cities found.</CommandEmpty>)}
-        <CommandGroup heading="Favorites ">
+        {/* <CommandGroup heading="Favorites ">
           <CommandItem>Calendar</CommandItem>
           
-        </CommandGroup>
+        </CommandGroup> */}
         
-        <CommandSeparator/>
+        
 
-        <CommandGroup heading="Recent Searches ">
-          <CommandItem>Calendar</CommandItem>
+        {history.length > 0 && (
+          <>
+         <CommandSeparator/>
+        <CommandGroup>
+          <div className="flex items-center justify-between px-2 my-2">
+            <p className="text-sm text-muted-foreground">Recent Searches</p>
+            <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => clearHistory.mutate()}
+            >
+              <XCircle className="h-4 w-4"/>
+              Clear
+            </Button>
+          </div>
+          {history.map((location)=>{
+           return (
+           <CommandItem 
+                    key={`${location.lat}-${location.lon}`}
+                    value={`${location.lat}|${location.lon}|${location.name}|${location.country}`}
+                    onSelect={handleSelect}
+                    >
+                        <Clock className="h-4 w-4 mr-2" />
+                        <span>{location.name}</span>
+                        {location.state && (
+                            <span className="text-muted-foreground ml-2">
+                                , {location.state}
+                            </span>
+
+                )}
+                        <span className="text-muted-foreground ml-2">
+                            , {location.country}
+                        </span>
+                        <span>{format(location.searchedAt,"MMM d, h:mm a")}</span>
+                        </CommandItem>
+          )})}
           
         </CommandGroup>
+        </>)}
 
         <CommandSeparator/>
 
---
+++ src/hooks/UseLocalStorage.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+     const [storedValue,setStoredValue] = useState<T>(()=>{
+           try{
+
+                const item = window.localStorage.getItem(key);
+                return item ? JSON.parse(item) : initialValue;
+
+           }catch(err){
+                 console.log(err);
+                 return initialValue;
+           }
+       }); 
+
+       useEffect(() => {
+           try{
+             window.localStorage.setItem(key,JSON.stringify(storedValue));
+           }catch(err){
+               console.error(`Error setting localStorage key "${key}":`, err);
+           }
+       },[key,storedValue])
+
+       return [storedValue, setStoredValue] as const;
+}
